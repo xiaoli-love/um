a6f00b476864d9498ee8dd9608137f8df4f9b7a5
小立 <119867083+xiaoli-love@users.noreply.github.com>
2024-09-22 21:51:20 +0800

    Add files via upload

---
+++ precache-manifest.a3ff6f1004dbd29bf0f4c20b201d3601.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696e3e433a1ec0ce3dc89eb4da395337",
+    "url": "3459963cc1601ca7b9aa.worker.js"
+  },
+  {
+    "revision": "8ae71f7a0bc1bfe2e90b",
+    "url": "css/app.ef4a7eb1.css"
+  },
+  {
+    "revision": "df769fa4a716427b9d2d",
+    "url": "css/chunk-vendors.c0f0ef1d.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "2a6925dd37f6de3b5149ce596c8c602d",
+    "url": "img/logo_.gif"
+  },
+  {
+    "revision": "59928a5e383f1e4978cde11139ab5313",
+    "url": "index.html"
+  },
+  {
+    "revision": "2d12777f2703612307ff4a12f1b21899",
+    "url": "ixarea-stats.js"
+  },
+  {
+    "revision": "8ae71f7a0bc1bfe2e90b",
+    "url": "js/app.a61c88ec.js"
+  },
+  {
+    "revision": "df769fa4a716427b9d2d",
+    "url": "js/chunk-vendors.2a290124.js"
+  },
+  {
+    "revision": "02995355b96ddf2519cd49f8aa73bb46",
+    "url": "loader.js"
+  },
+  {
+    "revision": "523b1a2eae8cb533fa6bd73831308f09",
+    "url": "static/kgm.mask"
+  },
+  {
+    "revision": "2a6925dd37f6de3b5149ce596c8c602d",
+    "url": "static/logo_.gif"
+  },
+  {
+    "revision": "f97730bdb4ab785c3c83a3e78b30d1d2",
+    "url": "web-manifest.json"
+  }
+]);
